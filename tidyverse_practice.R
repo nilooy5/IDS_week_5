3b7c01b156fbc04cf98c34bcba929cc1029c380b
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-25 14:20:45 +0600

    codes for tibble added

--- tidyverse_practice.R
+++ tidyverse_practice.R
@@ -1,3 +1,22 @@
 if(!("tidyverse" %in% rownames(installed.packages()))) {
   install.packages("tidyverse")
 }
+
+library("tidyverse")
+
+as_tibble(iris)
+
+# another way to create a tibble
+tbl_1 <- tibble(x=1:5,
+                y=1,  # recycling feature is allowed in a column
+                z=x^2+y)
+tbl_1
+
+# transposed tibble; "tribble"
+tbl_2 <- tribble(
+  ~x, ~y, ~z,
+  "a", 2, 3.6,
+  "b", 1, 8.5
+)
+
+tbl_2
